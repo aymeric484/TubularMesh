692ea3208080baad4a4e4207ec44392b9464c7f0
Your Name <giraud@pastis.u-strasbg.fr>
2016-05-04 13:23:15 +0200

    Normales fixed

--- branch.cpp
+++ branch.cpp
@@ -13,7 +13,6 @@ Branch::Branch()
     Vec4 V_externe_begin(0.0, 0.0, 0.0, 1.0);
     Vec4 V_externe_end(0.5, 0.5, 8.0, 1.0);
 
-
     articulations_.push_back(V40);
     articulations_.push_back(V41);
     articulations_.push_back(V42);
@@ -23,7 +22,6 @@ Branch::Branch()
 
     articulation_externe_begin_ = V_externe_begin;
     articulation_externe_end_ = V_externe_end;
-
 
     branch_size_= articulations_.size();
 
@@ -37,103 +35,100 @@ void Branch::GetAxisFromBranch()
     ComputeN();
     ComputeB();
 
-/*    for(int i = 0; i < branch_size_; i++)
-        std::cout << courbure_[i] << std::endl;*/
+    /*
+    for(int i = 0; i < branch_size_; i++)
+    {
+        std::cout << " iteration " << i << std::endl;
+        std::cout << " tangente " << T_axis_[i][0] << " ; " << T_axis_[i][1] << " ; " << T_axis_[i][2] << std::endl;
+        std::cout << " normale " << N_axis_[i][0] << " ; " << N_axis_[i][1] << " ; " << N_axis_[i][2] << std::endl;
+        std::cout << " B " << B_axis_[i][0] << " ; " << B_axis_[i][1] << " ; " << B_axis_[i][2] << std::endl;
+        std::cout << " courbure " << courbure_[i] << std::endl;
+    }*/
+
 
 }
 
 void Branch::ComputeT()
 {
-    /*
-    for(std::vector<Vec4>::iterator it = articulations_.begin(); it != articulations_.end(); ++it)
-    {
-       if(it != articulations_.end())
-           T_axis_.push_back( (*(std::next(it, 1))).head<3>() - (*it).head<3>());
-       else{
-           T_axis_.push_back( articulation_externe_.head<3>() - (*it).head<3>());
-           std::cout << "here" << std::endl;
-       }
-    }*/
+    Vec3 normalized_tan;
 
-    Vec3 normalized_vec3;
-
-    normalized_vec3 = (articulations_[1].head<3>() - articulation_externe_begin_.head<3>())/2;
-    normalized_vec3 = normalized_vec3/sqrt(normalized_vec3[0]*normalized_vec3[0] + normalized_vec3[1]*normalized_vec3[1] + normalized_vec3[2]*normalized_vec3[2]);
-    T_axis_.push_back(normalized_vec3);
+    normalized_tan = (articulations_[1].head<3>() - articulation_externe_begin_.head<3>())/2;
+    normalized_tan = normalized_tan/sqrt(normalized_tan[0]*normalized_tan[0] + normalized_tan[1]*normalized_tan[1] + normalized_tan[2]*normalized_tan[2]);
+    T_axis_.push_back(normalized_tan);
 
     for( int i = 1; i < branch_size_ - 1; i++ )
     {
-        normalized_vec3 = (articulations_[i+1].head<3>() - articulations_[i-1].head<3>())/2;
-        normalized_vec3 = normalized_vec3/sqrt(normalized_vec3[0]*normalized_vec3[0] + normalized_vec3[1]*normalized_vec3[1] + normalized_vec3[2]*normalized_vec3[2]);
-        T_axis_.push_back(normalized_vec3);
-         //T_axis_.push_back( (articulations_[i+1].head<3>() - articulations_[i-1].head<3>())/2 );
+        normalized_tan = (articulations_[i+1].head<3>() - articulations_[i-1].head<3>())/2;
+        normalized_tan = normalized_tan/sqrt(normalized_tan[0]*normalized_tan[0] + normalized_tan[1]*normalized_tan[1] + normalized_tan[2]*normalized_tan[2]);
+        T_axis_.push_back(normalized_tan);
     }
 
-    normalized_vec3 = (articulation_externe_end_.head<3>() - articulations_[branch_size_-2].head<3>())/2;
-    normalized_vec3 = normalized_vec3/sqrt(normalized_vec3[0]*normalized_vec3[0] + normalized_vec3[1]*normalized_vec3[1] + normalized_vec3[2]*normalized_vec3[2]);
-    T_axis_.push_back(normalized_vec3);
-    //T_axis_.push_back( (articulation_externe_end_.head<3>() - articulations_[branch_size_-2].head<3>())/2 );
+    normalized_tan = (articulation_externe_end_.head<3>() - articulations_[branch_size_-2].head<3>())/2;
+    normalized_tan = normalized_tan/sqrt(normalized_tan[0]*normalized_tan[0] + normalized_tan[1]*normalized_tan[1] + normalized_tan[2]*normalized_tan[2]);
+    T_axis_.push_back(normalized_tan);
 }
 
 void Branch::ComputeN()
 {
-    /*
-    for(std::vector<Vec3>::iterator it = T_axis_.begin(); it != T_axis_.end(); ++it)
-    {
-       if(it != T_axis_.end())
-           N_axis_.push_back( *(std::next(it, 1)) - *it);
-       else{
-           N_axis_.push_back( *it - *(std::prev(it, 1)));
-           std::cout << "here" << std::endl;
-       }
+    Vec3 normalized_nor;
+    Vec3 normalized_tan_prec;
+    Vec3 normalized_tan_suiv;
 
-    }
-    */
-
-    /*
-    N_axis_.push_back( T_axis_[1] - T_axis_[0]);
-
-    for( int i = 1; i < branch_size_ - 1; i++ )
-        N_axis_.push_back( (T_axis_[i+1].head<3>() - T_axis_[i-1].head<3>())/2);
-
-    N_axis_.push_back( T_axis_[branch_size_ - 1] - T_axis_[branch_size_ -2]);
-    */
-
-    Vec3 normalized_vec3;
-
-    normalized_vec3 = articulations_[1].head<3>() + articulation_externe_begin_.head<3>() - 2*articulations_[0].head<3>();
-    courbure_.push_back(sqrt(normalized_vec3[0]*normalized_vec3[0] + normalized_vec3[1]*normalized_vec3[1] + normalized_vec3[2]*normalized_vec3[2]));
-    normalized_vec3 = normalized_vec3/courbure_[0];
-    N_axis_.push_back(normalized_vec3);
-    //N_axis_.push_back( articulations_[1].head<3>() + articulation_externe_begin_.head<3>() - 2*articulations_[0].head<3>());
+    //normalisation de la tangente T(-1)
+    normalized_tan_prec = articulations_[0].head<3>() - articulation_externe_begin_.head<3>();
+    normalized_tan_prec = normalized_tan_prec/sqrt(normalized_tan_prec[0] * normalized_tan_prec[0] + normalized_tan_prec[1] * normalized_tan_prec[1] + normalized_tan_prec[2] * normalized_tan_prec[2]);
+    //normalisation de la tangente T(0)
+    normalized_tan_suiv = articulations_[1].head<3>() - articulations_[0].head<3>();
+    normalized_tan_suiv = normalized_tan_suiv/sqrt(normalized_tan_suiv[0] * normalized_tan_suiv[0] + normalized_tan_suiv[1] * normalized_tan_suiv[1] + normalized_tan_suiv[2] * normalized_tan_suiv[2]);
+    //Calcul de la normale N(0) + normalisation et calcul de la courbure
+    normalized_nor = normalized_tan_suiv - normalized_tan_prec;
+    courbure_.push_back(sqrt(normalized_nor[0]*normalized_nor[0] + normalized_nor[1]*normalized_nor[1] + normalized_nor[2]*normalized_nor[2]));
+    normalized_nor = normalized_nor/courbure_[0];
+    N_axis_.push_back(normalized_nor);
 
     for( int i = 1; i < branch_size_ - 1; i++ )
     {
-        normalized_vec3 = articulations_[i+1].head<3>() + articulations_[i-1].head<3>() - 2*(articulations_[i].head<3>());
-        courbure_.push_back(sqrt(normalized_vec3[0]*normalized_vec3[0] + normalized_vec3[1]*normalized_vec3[1] + normalized_vec3[2]*normalized_vec3[2]));
-        normalized_vec3 = normalized_vec3/courbure_[i];
-        N_axis_.push_back(normalized_vec3);
-        //N_axis_.push_back( articulations_[i+1].head<3>() + articulations_[i-1].head<3>() - 2*(articulations_[i].head<3>()));
+        //normalisation de la tangente T(i-1)
+        normalized_tan_prec = articulations_[i-1].head<3>() - articulations_[i].head<3>();
+        normalized_tan_prec = normalized_tan_prec/sqrt(normalized_tan_prec[0] * normalized_tan_prec[0] + normalized_tan_prec[1] * normalized_tan_prec[1] + normalized_tan_prec[2] * normalized_tan_prec[2]);
+        //normalisation de la tangente T(i)
+        normalized_tan_suiv = articulations_[i+1].head<3>() - articulations_[i].head<3>();
+        normalized_tan_suiv = normalized_tan_suiv/sqrt(normalized_tan_suiv[0] * normalized_tan_suiv[0] + normalized_tan_suiv[1] * normalized_tan_suiv[1] + normalized_tan_suiv[2] * normalized_tan_suiv[2]);
+        //Calcul de la normale + normalisation et calcul de la courbure
+        normalized_nor = normalized_tan_suiv - normalized_tan_prec;
+        courbure_.push_back(sqrt(normalized_nor[0]*normalized_nor[0] + normalized_nor[1]*normalized_nor[1] + normalized_nor[2]*normalized_nor[2]));
+        normalized_nor = normalized_nor/courbure_[i];
+        N_axis_.push_back(normalized_nor);
     }
 
-    normalized_vec3 = articulations_[branch_size_ - 2].head<3>() + articulation_externe_end_.head<3>() - 2*(articulations_[branch_size_ - 1].head<3>());
-    courbure_.push_back(sqrt(normalized_vec3[0]*normalized_vec3[0] + normalized_vec3[1]*normalized_vec3[1] + normalized_vec3[2]*normalized_vec3[2]));
-    normalized_vec3 = normalized_vec3/courbure_[branch_size_ -1];
-    N_axis_.push_back(normalized_vec3);
-    //N_axis_.push_back( articulations_[branch_size_ - 2].head<3>() + articulation_externe_end_.head<3>() - 2*(articulations_[branch_size_ - 1].head<3>()));
+    //normalisation de la tangente T(size -2)
+    normalized_tan_prec = articulations_[branch_size_ - 1].head<3>() - articulations_[branch_size_ - 2].head<3>();
+    normalized_tan_prec = normalized_tan_prec/sqrt(normalized_tan_prec[0] * normalized_tan_prec[0] + normalized_tan_prec[1] * normalized_tan_prec[1] + normalized_tan_prec[2] * normalized_tan_prec[2]);
+    //normalisation de la tangente T(size -1)
+    normalized_tan_suiv = articulation_externe_end_.head<3>() - articulations_[branch_size_ - 1].head<3>();
+    normalized_tan_suiv = normalized_tan_suiv/sqrt(normalized_tan_suiv[0] * normalized_tan_suiv[0] + normalized_tan_suiv[1] * normalized_tan_suiv[1] + normalized_tan_suiv[2] * normalized_tan_suiv[2]);
+    //Calcul de la normale N(size-1) + normalisation et calcul de la courbure
+    normalized_nor = normalized_tan_suiv - normalized_tan_prec;
+    courbure_.push_back(sqrt(normalized_nor[0]*normalized_nor[0] + normalized_nor[1]*normalized_nor[1] + normalized_nor[2]*normalized_nor[2]));
+    normalized_nor = normalized_nor/courbure_[branch_size_ -1];
+    N_axis_.push_back(normalized_nor);
 
 }
 
 void Branch::ComputeB()
 {
+    /* Pour calculer la torsion, utiliser l egalité B' = -torsion_ *N
+      Donc, avec B' = (B(i+1)-B(i-1))/2 => dérivée approximée
+      */
+
     Vec3 normalized_vec3;
 
     for( int i = 0 ; i < T_axis_.size() ; i++ )
     {
+
         normalized_vec3 = T_axis_[i].cross(N_axis_[i]);
         normalized_vec3 = normalized_vec3/sqrt(normalized_vec3[0]*normalized_vec3[0] + normalized_vec3[1]*normalized_vec3[1] + normalized_vec3[2]*normalized_vec3[2]);
         B_axis_.push_back(normalized_vec3);
-        //B_axis_.push_back(T_axis_[i].cross(N_axis_[i]));
     }
 
 }
